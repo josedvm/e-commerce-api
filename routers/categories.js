a71b19ff79a2cbcec0fef1f3946a61e6eb72301c
josedvm <jose.mamian@correounivalle.edu.co>
2021-04-02 16:07:59 -0500

    delete category by id added

--- routers/categories.js
+++ routers/categories.js
@@ -25,7 +25,22 @@ router.post('/', async(req, res) =>{
     return res.status(500).send('the category could not be created.')
 
     res.send(newCategory)
-
 })
+
+//delete
+router.delete('/:id', (req, res) =>{
+    Category.findByIdAndRemove(req.params.id)
+    .then(category =>{
+        if(category){
+            return res.status(200).json({succes:true,message:`Category with the id: ${req.params.id} deleted.`})
+        }else{
+            return res.status(404).json({succes:false,message:`Category not found.`})
+        }
+    }).catch(err => {
+        return res.status(400).json({succes:false ,error:err})
+    })
+
+    })
+
 
 module.exports = router;
