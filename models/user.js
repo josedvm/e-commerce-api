b7a14fd2b2aac585337024ea173a16fd2b858467
josedvm <jose.mamian@correounivalle.edu.co>
2021-04-04 12:30:54 -0500

    update the user model with the correct information

--- models/user.js
+++ models/user.js
@@ -2,11 +2,45 @@ const mongoose = require("mongoose");
 
 /* scheme */
 const userScheme = mongoose.Schema({
-	name: String,
-	image: String,
-	countInStock: {
-		type: Number,
+	name: {
+		type: String,
 		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+	},
+	passwordHash: {
+		type: String,
+		required: true,
+	},
+	phone: {
+		type: String,
+		required: true,
+	},
+	street: {
+		type: String,
+		default: "",
+	},
+	apartment: {
+		type: String,
+		default: "",
+	},
+	zip: {
+		type: String,
+		default: "",
+	},
+	city: {
+		type: String,
+		default: "",
+	},
+	country: {
+		type: String,
+		default: "",
+	},
+	isAdmin: {
+		type: Boolean,
+		default: false,
 	},
 });
 
