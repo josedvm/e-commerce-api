f0116eeb1645f200938e4ec4c034db18bc6eb272
josedvm <jose.mamian@correounivalle.edu.co>
2021-04-04 12:49:19 -0500

    method post to creater an user was added

--- routers/users.js
+++ routers/users.js
@@ -1,14 +1,46 @@
-const express = require('express')
-const router = express.Router()
-const {User} = require('../models/user')
+const express = require("express");
+const router = express.Router();
+const { User } = require("../models/user");
 
 //get
-router.get(`/`,async (req,res) =>{
-    const userList = await User.find()
-    if(!userList){
-        res.status(500).json({error:'something was wrong!'})
-    }
-    res.send(userList)    
-})
+router.get(`/`, async (req, res) => {
+	const userList = await User.find();
+	if (!userList) {
+		res.status(500).json({ error: "something was wrong!" });
+	}
+	res.send(userList);
+});
+
+//post - create a suer
+router.post("/", async (req, res) => {
+	const {
+		name,
+		email,
+		passwordHash,
+		phone,
+		street,
+		apartment,
+		zip,
+		city,
+		country,
+		isAdmin,
+	} = req.body;
+	let newUser = new User({
+		name,
+		email,
+		passwordHash,
+		phone,
+		street,
+		apartment,
+		zip,
+		city,
+		country,
+		isAdmin,
+	});
+	newUser = await newUser.save();
+	if (!newUser) return res.status(500).send("the user could not be created.");
+
+	res.send(newUser);
+});
 
 module.exports = router;
